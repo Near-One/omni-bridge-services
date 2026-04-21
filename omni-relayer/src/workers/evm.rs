4afaa5314a45f94ef80a294bbf6e4c039c8243f7
karim-en <karim.alabtakh@nearone.org>
2026-04-21 23:36:50 +0100

    fix: clippy

--- omni-relayer/src/workers/evm.rs
+++ omni-relayer/src/workers/evm.rs
@@ -52,7 +52,8 @@ pub async fn process_init_transfer_event(
 
     if current_timestamp < creation_timestamp + expected_finalization_time {
         let remaining =
-            (creation_timestamp + expected_finalization_time - current_timestamp) as u64;
+            u64::try_from(creation_timestamp + expected_finalization_time - current_timestamp)
+                .unwrap_or(0);
         return Ok(EventAction::RetryAfter(Duration::from_secs(remaining)));
     }
 
@@ -329,7 +330,8 @@ pub async fn process_evm_transfer_event(
 
     if current_timestamp < creation_timestamp + expected_finalization_time {
         let remaining =
-            (creation_timestamp + expected_finalization_time - current_timestamp) as u64;
+            u64::try_from(creation_timestamp + expected_finalization_time - current_timestamp)
+                .unwrap_or(0);
         return Ok(EventAction::RetryAfter(Duration::from_secs(remaining)));
     }
 
@@ -468,7 +470,8 @@ pub async fn process_deploy_token_event(
 
     if current_timestamp < creation_timestamp + expected_finalization_time {
         let remaining =
-            (creation_timestamp + expected_finalization_time - current_timestamp) as u64;
+            u64::try_from(creation_timestamp + expected_finalization_time - current_timestamp)
+                .unwrap_or(0);
         return Ok(EventAction::RetryAfter(Duration::from_secs(remaining)));
     }
 
