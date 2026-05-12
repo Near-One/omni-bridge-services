e712a413c14c395886bf7989178bb622cd827384
Ivan Frolov <59515280+frolvanya@users.noreply.github.com>
2026-05-12 23:46:03 +0200

    feat(omni-relayer): added a delay before signing near->btc transfers (#22)
    * feat: delay before signing btc transaction
    
    This is done to avoid race with satoshi's relayer
    
    * feat: improved logging for near<->utxo transfers
    
    * refactor: mark as signed after finalization and clear on ttl
    
    * chore: add to redis only if `utxo_sign_delay_secs` > 0

--- omni-relayer/src/startup/event_handlers.rs
+++ omni-relayer/src/startup/event_handlers.rs
@@ -586,8 +586,19 @@ pub(super) async fn handle_transaction_event(
             destination_chain,
             relayer,
         } => {
+            let btc_pending_id = if let TransferIdKind::Utxo(utxo_id) =
+                event.transfer_id.kind.clone()
+            {
+                Some(utxo_id.tx_hash)
+            } else {
+                warn!(
+                    "UtxoSignTransaction missing TransferIdKind::Utxo, redis cleanup will be skipped: {origin_transaction_id}"
+                );
+                None
+            };
+
             info!(
-                "Received UtxoSignBtcTransaction on {:?}: {origin_transaction_id}",
+                "Received UtxoSignBtcTransaction on {:?}: {origin_transaction_id} (btc_pending_id={btc_pending_id:?})",
                 event.transfer_id.origin_chain
             );
             add_event(
@@ -600,6 +611,7 @@ pub(super) async fn handle_transaction_event(
                     chain: destination_chain,
                     near_tx_hash: origin_transaction_id.clone(),
                     relayer,
+                    btc_pending_id,
                 },
             )
             .await;
