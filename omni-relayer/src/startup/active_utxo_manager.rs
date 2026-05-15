b2b03d83dc3a7c15a8d2cc7053cda9cf5d848ea3
karim-en <karim.alabtakh@nearone.org>
2026-05-15 23:56:02 +0100

    chore: move to config

--- omni-relayer/src/startup/active_utxo_manager.rs
+++ omni-relayer/src/startup/active_utxo_manager.rs
@@ -61,12 +61,11 @@ pub async fn start_active_utxo_manager(
             }
         };
 
-        let fixed_fee_rate = Some(1000);
         match omni_connector
             .active_utxo_management(
                 chain,
-                fixed_fee_rate,
-                None,
+                settings.fixed_fee_rate,
+                settings.max_input_number,
                 TransactionOptions {
                     nonce,
                     wait_until: near_primitives::views::TxExecutionStatus::Final,
